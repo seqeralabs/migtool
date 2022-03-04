eb4c39e7f3b6569ffb5922499bf067f0551a975b
Jorge Aguilera <98739503+jorgeaguileraseqera@users.noreply.github.com>
2022-03-04 10:41:23 +0100

    feature: use graalvm to build linux runnable (#13)

--- src/main/java/io/seqera/migtool/App.java
+++ src/main/java/io/seqera/migtool/App.java
@@ -8,7 +8,21 @@ package io.seqera.migtool;
 public class App {
 
     public static void main(String[] args) {
-        MigTool tool = new MigTool();
+        if( args.length != 7){
+            System.out.println("invalid arguments");
+            System.out.println("user password url dialect driver locations pattern");
+            System.exit(-1);
+            return;
+        }
+        MigTool tool = new MigTool()
+                .withUser(args[0])
+                .withPassword(args[1])
+                .withUrl(args[2])
+                .withDialect(args[3])
+                .withDriver(args[4])
+                .withLocations(args[5])
+                .withClassLoader(App.class.getClassLoader())
+                .withPattern(args[6]);
         tool.run();
     }
 }
