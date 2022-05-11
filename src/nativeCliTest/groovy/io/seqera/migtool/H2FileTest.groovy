5b5d189930371eade3a8a34cd1b59292214d7993
Jorge Aguilera <98739503+jorgeaguileraseqera@users.noreply.github.com>
2022-05-11 19:40:03 +0200

    feature: initialize h2 in native image (#22)

---
+++ src/nativeCliTest/groovy/io/seqera/migtool/H2FileTest.groovy
@@ -0,0 +1,49 @@
+package io.seqera.migtool
+
+import groovy.sql.Sql
+import spock.lang.Requires
+import spock.lang.Specification
+import spock.lang.Timeout
+
+import java.nio.file.Files
+import java.nio.file.Path
+
+@Timeout(30)
+@Requires({System.getenv('NATIVE_BINARY_PATH')})
+class H2FileTest extends Specification {
+
+    Path dbName;
+
+    def setup() {
+        dbName = Files.createTempFile("db_${UUID.randomUUID().toString()}","db")
+    }
+
+    def 'should run native binary' () {
+        given:
+        def BIN = System.getenv('NATIVE_BINARY_PATH')
+        def CLI = [BIN,
+                '-u', 'sa',
+                '-p', '',
+                '--url', "jdbc:h2:$dbName".toString(),
+                '--pattern', '^V(\\d+)__(.+)',
+                '--location', 'file:src/test/resources/migrate-db/h2' ]
+
+        when:
+        println "Running: ${CLI.join( )}"
+        def proc = new ProcessBuilder()
+                .command(CLI)
+                .redirectErrorStream(true)
+                .start()
+        and:
+        def result = proc.waitFor()
+        if( result!=0 )
+            System.err.println(proc.text)
+        
+        then:
+        result == 0
+
+        and:
+        Sql.newInstance("jdbc:h2:$dbName", "sa", "")
+                .rows("SELECT count(*) from license").size() == 1
+    }
+}
