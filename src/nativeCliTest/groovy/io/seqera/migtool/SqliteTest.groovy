5ef290ddae2151816fc5e5c33303f64d7179a8d4
Pedro Geadas <pedro.geadas@seqera.io>
2024-01-23 23:57:17 +0100

    Small tweaks

--- src/nativeCliTest/groovy/io/seqera/migtool/SqliteTest.groovy
+++ src/nativeCliTest/groovy/io/seqera/migtool/SqliteTest.groovy
@@ -1,53 +1,59 @@
 package io.seqera.migtool
 
-import groovy.sql.Sql
-import org.testcontainers.containers.MySQLContainer
 import spock.lang.Requires
-import spock.lang.Shared
 import spock.lang.Specification
 import spock.lang.Timeout
 
 import java.nio.file.Files
 import java.nio.file.Path
 
+import groovy.sql.Sql
 
 @Timeout(30)
-@Requires({System.getenv('NATIVE_BINARY_PATH')})
+@Requires({ System.getenv('NATIVE_BINARY_PATH') })
 class SqliteTest extends Specification {
 
     Path dbName;
 
     def setup() {
-        dbName = Files.createTempFile("db_${UUID.randomUUID().toString()}","db")
+        dbName = Files.createTempFile(
+                "db_${UUID.randomUUID().toString()}",
+                "db"
+        )
     }
 
-    def 'should run native binary' () {
+    def 'should run native binary'() {
         given:
         def BIN = System.getenv('NATIVE_BINARY_PATH')
         def CLI = [BIN,
-                '-u', 'user',
-                '-p', 'pass',
-                '--driver', 'org.sqlite.JDBC',
-                '--url', "jdbc:sqlite:/$dbName".toString(),
-                '--pattern', '^V(\\d+)__(.+)',
-                '--location', 'file:src/nativeCliTest/resources/migrate-db/sqlite' ]
+                   '-u', 'user',
+                   '-p', 'pass',
+                   '--driver', 'org.sqlite.JDBC',
+                   '--url', "jdbc:sqlite:/$dbName".toString(),
+                   '--pattern', '^V(\\d+)__(.+)',
+                   '--location', 'file:src/nativeCliTest/resources/migrate-db/sqlite']
 
         when:
-        println "Running: ${CLI.join( )}"
+        println "Running: ${CLI.join()}"
         def proc = new ProcessBuilder()
                 .command(CLI)
                 .redirectErrorStream(true)
                 .start()
         and:
         def result = proc.waitFor()
-        if( result!=0 )
+        if (result != 0) {
             System.err.println(proc.text)
-        
+        }
+
         then:
         result == 0
 
         and:
-        Sql.newInstance("jdbc:sqlite:/$dbName", "user", "pass")
-                .rows("SELECT count(*) from license").size() == 1
+        Sql.newInstance(
+                "jdbc:sqlite:/$dbName",
+                "user",
+                "pass"
+        )
+           .rows("SELECT count(*) from license").size() == 1
     }
 }
